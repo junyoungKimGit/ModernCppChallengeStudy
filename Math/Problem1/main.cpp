3b8e4deae19a4719eb1349f1a5f4853151cf3505
junyoungKimGit <jykim0824.snu@gmail.com>
2018-10-20 00:14:51 +0900

    solve p1

--- Math/Problem1/main.cpp
+++ Math/Problem1/main.cpp
@@ -1,6 +1,32 @@
-#include <gsl/gsl>
+//#include <gsl/gsl>
+#include <iostream>
+
+using namespace std;
+
+int gSum = 0;
 
 int main(int argc, char* argv[])
 {
+	int sEndNumber = 0;
+
+	cin >> sEndNumber;
+
+	for (int i = 3; i < sEndNumber; i = i + 3)
+	{
+		gSum += i;
+	}
+
+	for (int i = 5; i < sEndNumber; i = i + 5)
+	{
+		gSum += i;
+	}
+
+	for (int i = 15; i < sEndNumber; i = i + 15)
+	{
+		gSum -= i;
+	}
+
+	cout << gSum << endl;
+
     return 0;
 }
